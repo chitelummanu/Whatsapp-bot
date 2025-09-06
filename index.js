6ce8b5196466da6b810c4052e8146fc85dd25ee9
chitelummanu <chitelummanu@gmail.com>
2025-09-06 17:51:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,3 +76,22 @@ client.on('message', message => {
     if (message.from.includes('-')) return; // ignores groups
     // handle personal messages
 });
+
+const fs = require('fs');
+const SESSION_FILE_PATH = './session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({ session: sessionCfg });
+
+client.on('ready', () => {
+    console.log('Bot is ready!');
+});
+
+client.on('authenticated', (session) => {
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) console.error(err);
+    });
+});
